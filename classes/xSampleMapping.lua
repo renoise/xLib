5299c1ef4895ed90aeffa60ec90965f5e83e97d0
bjorn-nesby <bjorn.nesby@gmail.com>
2018-06-28 11:29:36 +0200

    - `xSampleMapping.shift_keyzone_by_semitones` refactored to `xKeyZone.shift_by_semitones`

--- classes/xSampleMapping.lua
+++ classes/xSampleMapping.lua
@@ -28,21 +28,3 @@ function xSampleMapping.within_note_range(note,mapping)
   return (note >= rng[1]) and (note <= rng[2]) 
 end
 
----------------------------------------------------------------------------------------------------
--- [Static] Shift samples by amount of semitones, starting from the sample index 
--- @param instr (renoise.Instrument)
--- @param sample_idx_from (int)
--- @param amt (int)
-
-function xSampleMapping.shift_keyzone_by_semitones(instr,sample_idx_from,amt)
-  TRACE("xSampleMapping.shift_keyzone_by_semitones(instr,sample_idx_from,amt)",instr,sample_idx_from,amt)
-
-  for sample_idx = sample_idx_from,#instr.samples do
-    local sample = instr.samples[sample_idx]
-    local smap = sample.sample_mapping
-    smap.base_note = smap.base_note+amt
-    smap.note_range = {smap.note_range[1]+amt, smap.note_range[2]+amt}
-  end
-
-end
-
